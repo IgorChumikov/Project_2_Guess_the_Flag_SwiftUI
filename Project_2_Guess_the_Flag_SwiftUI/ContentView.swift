ad3324d22c9aa3c65092b86f707adb28708aa319
IgorChumikov <chumikov.igor@mail.ru>
2022-02-13 21:37:20 +0300

    add image

--- Project_2_Guess_the_Flag_SwiftUI/ContentView.swift
+++ Project_2_Guess_the_Flag_SwiftUI/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     
+    
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack(spacing: 30) {
+            VStack {
+                Text("Tap the flag of")
+                Text(countries[correctAnswer])
+            }
+            ForEach(0..<3) { number in
+                Button {
+                    print("tap")
+                } label: {
+                    Image(self.countries[number])
+                        .renderingMode(.original)
+                }
+                
+            }
+        }
     }
 }
 
