8cd5c467f1f26684f31497545df3ab4223a65bde
IgorChumikov <chumikov.igor@mail.ru>
2022-02-13 20:54:58 +0300

    Initial Commit

---
+++ Project_2_Guess_the_Flag_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project_2_Guess_the_Flag_SwiftUI
+//
+//  Created by Игорь Чумиков on 13.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project_2_Guess_the_Flag_SwiftUI/Project_2_Guess_the_Flag_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_2_Guess_the_Flag_SwiftUIApp.swift
+//  Project_2_Guess_the_Flag_SwiftUI
+//
+//  Created by Игорь Чумиков on 13.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Project_2_Guess_the_Flag_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
